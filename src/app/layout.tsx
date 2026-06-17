ba329c52eac765d738a1adf650e864cc2973a67f
Kyndel Roy <k.bangoy.suarez@gmail.com>
2026-06-17 23:17:08 +0800

    refactor: remove unused cn utility from root layout and simplify html class declaration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,6 @@ import type { Metadata } from 'next'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 
-import { cn } from '@/lib/utils'
-
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -91,7 +89,7 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
-    <html lang='en' data-scroll-behavior='smooth' className={cn('flex min-h-full w-full scroll-smooth antialiased')}>
+    <html lang='en' data-scroll-behavior='smooth' className='flex min-h-full w-full scroll-smooth antialiased'>
       <body className='flex min-h-full w-full flex-auto flex-col bg-stone-100'>
         <TooltipProvider>{children}</TooltipProvider>
       </body>
